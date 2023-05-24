ffb6d1ee5c9628c36a4b9773dbc6f38feca7856d
Lia-Ramirez <111774024+Lia-Ramirez@users.noreply.github.com>
2023-05-24 16:03:25 -0600

    Reportes

---
+++ ReportesLIaRamirez/RegistrosHR/RegistrosHR/formularios/FrmVistaPrevia.vb
@@ -0,0 +1,8 @@
+﻿Public Class FrmVistaPrevia
+    Private Sub FrmVistaPrevia_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'DsDBHR.RptJobEmployees' table. You can move, or remove it, as needed.
+        Me.RptJobEmployeesTableAdapter.Fill(Me.DsDBHR.RptJobEmployees)
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+End Class
